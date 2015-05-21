4b284da2a3ad0bbfdf22f553a40718120ad4111a
Xavier Gonzalez <xavier@gonzalez.pm>
2015-05-21 18:39:13 +0200

    une route dynamique pour les coureurs dynamique en asynchrone

--- public_html/js/app/coureurs/controllers.js
+++ public_html/js/app/coureurs/controllers.js
@@ -4,7 +4,8 @@
     //le ['$scope', est ajouter au cas ou on minififirait le script
     // afin que scope ne soit pas écrasé mais inutile si gardé en clair.
     app.controller('CoureursController', ['$scope', 'getTousCoureurs',function($scope,getTousCoureurs){
-        $scope.coureurs = getTousCoureurs;
+        $scope.data = getTousCoureurs;
+        
     }]);
 
     app.controller('CoureurController', ['$scope', '$route' , 'getUnCoureur',function($scope,$route,getUnCoureur){
