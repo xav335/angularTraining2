2147cb897d7a4c0d98d00d194c15a8608bf5126e
Xavier Gonzalez <xavier@gonzalez.pm>
2015-05-22 16:25:54 +0200

    asynchronisme de plusieurs services chainés fin

--- public_html/js/app/app.js
+++ public_html/js/app/app.js
@@ -21,22 +21,50 @@
             templateUrl:'js/app/partials/fiche.html',
             controller:'CoureurController',
             resolve:{
-                unCoureur:['$route','getUnCoureur','getParticipationsUnCoureur','$q', function($route,getUnCoureur,getParticipationsUnCoureur,$q){
+                unCoureur:['$route','getUnCoureur','getParticipationsUnCoureur','getSomeRuns','getSomeEvents','$q', 
+                        function($route,getUnCoureur,getParticipationsUnCoureur,getSomeRuns,getSomeEvents,$q){
                     var id = $route.current.params.id;
                         //return getUnCoureur($route.current.params.id);    
                     var differe = $q.defer();
                     var toutesPromesses = $q.all([getUnCoureur(id), getParticipationsUnCoureur(id)]);
+                    var coureurComplet = {};
                     
                     toutesPromesses.then(function(tousResultats){
-                        var coureurComplet = tousResultats[0];
+                        coureurComplet = tousResultats[0];
                         coureurComplet.results = tousResultats[1];
+                        //return coureurComplet;
+                    }).then(function(){
+                        var ids ='';
+                        angular.forEach(coureurComplet.results, function(participation){
+                            ids = ids + participation.runId +',';
+                        });
+                        return getSomeRuns(ids);
+                       
+                    }).then(function(courses){
+                        var eventIds = [];
+                        angular.forEach(coureurComplet.results, function(participation,index){
+                            participation.runDistance = courses[index].distance;
+                            eventIds.push(courses[index].eventId);
+                        });
+                        
+                        //console.log(eventIds);
+                        return getSomeEvents(eventIds.join(','));
+                        
+                    }).then(function(evenements){
+                        angular.forEach(coureurComplet.results, function(participation,index){
+                            participation.eventName = evenements[index].name;
+                            participation.eventDate = evenements[index].date.date.substr(0,10);
+                        });
+                        
+                        
                         differe.resolve(coureurComplet);
+                    }).catch(function(error){
+                        differe.reject('Impossible de réassembler le coureur '+ error);
                     });
                     return differe.promise;
                 }]
             }
         })
-        
         .otherwise('/'); 
 
     }]);
--- public_html/js/app/coureurs/services.js
+++ public_html/js/app/coureurs/services.js
@@ -15,6 +15,14 @@
         return $resource('/api/runners/:id/entrants',{id:'@runnerId'});    
     }]);
 
+    app.service('Courses',['$resource', function($resource){
+        return $resource('/api/runs/:id',{id:'@id'});    
+    }]);
+
+    app.service('Evenements',['$resource', function($resource){
+        return $resource('/api/events/:id',{id:'@id'});    
+    }]);
+
     app.factory('getTousCoureurs',['Coureurs','$q','CreeNomComplet', function(Coureurs,$q,CreeNomComplet){
         var differe = $q.defer();
         
@@ -47,9 +55,21 @@
         return function (id){
             //si transformation du runTime passer par $q
             //var differe = $q.defer();
-             return ParticipationsCoureur.query({id:id});
+             return ParticipationsCoureur.query({id:id}).$promise;
         };
     }]);
+
+    app.factory('getSomeRuns', ['Courses','$q',function(Courses,$q){
+        return function (ids){
+             return Courses.query({id:ids}).$promise;
+        };
+    }]);   
+
+    app.factory('getSomeEvents', ['Evenements','$q',function(Evenements,$q){
+        return function (ids){
+             return Evenements.query({id:ids}).$promise;
+        };
+    }]); 
 /*
     app.factory ('getTousCoureurs', ['$http','CreeNomComplet', function($http,CreeNomComplet){
         //var toto = [{"id":"1","firstName":"Jean-Marc","lastName":"Martin","gender":"M","birthDate":{"date":"1974-03-02 00:00:00.000000","timezone_type":3,"timezone":"Europe\/Berlin"},"ffaId":"PR123654","picture":null},{"id":"2","firstName":"Ir\u00e8ne","lastName":"Cazaux","gender":"F","birthDate":{"date":"1975-06-15 00:00:00.000000","timezone_type":3,"timezone":"Europe\/Berlin"},"ffaId":"L987654","picture":null},{"id":"4","firstName":"Annie","lastName":"Petit","gender":"J","birthDate":{"date":"1972-06-11 00:00:00.000000","timezone_type":3,"timezone":"Europe\/Berlin"},"ffaId":"L000000","picture":null},{"id":"103","firstName":"John","lastName":"Smith","gender":"M","birthDate":{"date":"1977-07-15 00:00:00.000000","timezone_type":3,"timezone":"Europe\/Berlin"},"ffaId":"","picture":null},{"id":"154","firstName":"Benjamin","lastName":"Button","gender":"M","birthDate":{"date":"1978-05-06 00:00:00.000000","timezone_type":3,"timezone":"Europe\/Berlin"},"ffaId":null,"picture":null},{"id":"155","firstName":"Milo","lastName":"Gentile","gender":"M","birthDate":{"date":"1974-03-23 00:00:00.000000","timezone_type":3,"timezone":"Europe\/Berlin"},"ffaId":null,"picture":null}];
